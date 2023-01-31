098645dbc26dcaf9204a8af8cd876dd4e6d7acc7
Sang-hyun, Cho <cho911115@gmail.com>
2023-02-01 00:53:11 +0900

    fix: CORS 설정 (lambda url)

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -3,7 +3,8 @@ package middleware
 import "github.com/gin-gonic/gin"
 
 func SetHeader(c *gin.Context) {
-	c.Header("Access-Control-Allow-Origin", "*")
+	// lambda url에서 cors 설정을 할 경우 중복된 헤더 값으로 들어가서 클라이언트 에러 발생
+	//c.Header("Access-Control-Allow-Origin", "*")
 	c.Header("Cache-Control", "no-store")
 	c.Next()
 }
